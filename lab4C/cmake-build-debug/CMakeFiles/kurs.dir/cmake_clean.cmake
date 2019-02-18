2b7c2af6522672c335b88af8e49ff7b67d718efc
Renata <krr12.04.98@mail.ru>
2019-02-18 19:45:11 +0300

    coursework

---
+++ lab4C/cmake-build-debug/CMakeFiles/kurs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kurs.dir/main.cpp.o"
+  "kurs.pdb"
+  "kurs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kurs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
